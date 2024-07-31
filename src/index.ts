4229fe7c374e9b7d448b2871e228f205c52f747f
Oriana Mendez <egleemendez@gmail.com>
2024-07-31 11:33:24 +0200

    mock db and get endpoint /cars
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,47 @@
 import express, { NextFunction } from "express";
 import bunyan from "bunyan";
 import { v4 } from "uuid";
-import { serialize } from "v8";
 
 const log = bunyan.createLogger({
   name: "gcp-lab",
   serializers: bunyan.stdSerializers,
 });
 
+type Car = {
+  id: string;
+  status: "available" | "purchased";
+};
+
+const cars: Car[] = [
+  {
+    id: "1",
+    status: "available",
+  },
+  {
+    id: "2",
+    status: "purchased",
+  },
+  {
+    id: "3",
+    status: "available",
+  },
+];
+
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use((req: any, res, next: NextFunction) => {
   req.log = log.child({ req_id: v4() }, true);
-  req.log.info({ req: req });
-  res.on("finish", () => req.log.info({ res }));
+  req.log.info({ message: req.method, req: req });
+  res.on("finish", () => req.log.info({ message: "Response!", res }));
   next();
 });
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  throw new Error("NOT Hello world...");
-  //res.send("Hello World!");
-});
-
-app.get("/status", (req, res) => {
-  log.info({ message: "It's aliiive!" });
-  res.sendStatus(200);
-});
-
-app.post("/payments", (req, res) => {
-  log.info({ message: "Payment received", req: req });
-  res.json(req.body);
+app.get("/cars", (req: any, res: any) => {
+  req.log.info({ message: "GET /cars" });
+  res.json(cars);
 });
 
 app.listen(port, () => {
@@ -40,6 +49,6 @@ app.listen(port, () => {
 });
 
 app.use((err: any, req: any, res: any, next: NextFunction) => {
-  req.log.error({ err, message: err.message });
+  req.log.error({ message: err.message, err });
   res.status(500).json("Internal server error");
 });
